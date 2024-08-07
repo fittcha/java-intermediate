2c19a4adfd0ec1f1619632e63ec7a56e27d737fa
iwannabarmus <120002312+fittcha@users.noreply.github.com>
2024-08-07 17:51:45 +0900

    Add files via upload
    create

---
+++ java-mid1/src/main/java/com/csm/javamid1/JavaMid1Application.java
@@ -0,0 +1,13 @@
+package com.csm.javamid1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMid1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMid1Application.class, args);
+    }
+
+}
---
+++ java-mid1/src/test/java/com/csm/javamid1/JavaMid1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csm.javamid1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMid1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
